08b405b7bf340323a51a809e3c0a1ec2147a794a
Shahzeb K <shahzeb001@gmail.com>
2019-01-15 20:53:06 -0800

    Added the Location class via refactor

---
+++ src/map/Location.java
@@ -0,0 +1,21 @@
+package map;
+
+public class Location {
+	
+	private double lat;
+	private double lon;
+
+	public Location(double lat, double lon) {
+		this.lat = lat;
+		this.lon = lon;
+	}
+	
+	public double getLat() {
+		return lat;
+	}
+	
+	public double getLon() {
+		return lon;
+	}
+
+}
