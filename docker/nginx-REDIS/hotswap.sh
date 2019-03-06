29d1d1b744740c0680a63d0c9215e589d91f4d02
Shahzeb K <shahzeb001@gmail.com>
2019-03-06 00:45:23 -0800

    Make nginx config path it's own url + string compare

--- docker/nginx-REDIS/hotswap.sh
+++ docker/nginx-REDIS/hotswap.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
+# Set the nginx_config path:
+nginx_config="/etc/nginx/nginx.conf"
+
 # Check to make sure that an argument was passed in
 if [[ -z "$1" ]]; then
     echo "[error] usage: hotswap.sh <new redis url>"
@@ -8,18 +11,18 @@ if [[ -z "$1" ]]; then
 fi
 
 # Get the current redis url:
-current=$(sed -En 's/.*server\s(.+?):6379;.*/\1/p' /etc/nginx/nginx.conf)
+current=$(sed -En 's/.*server\s(.+?):6379;.*/\1/p' $nginx_config) 
 
-echo "[info] replacing $current with $1"
 
 # If statement for extra credit:
-if [[ "$current" -ne "$1" ]]; then
-# Replace the redis URL if the arg $1 url is different than $current
-    sed -ivE "s/server\s(.+?):6379/server $1:6379/" /etc/nginx/nginx.conf
+if [[ "$current" != "$1" ]]; then
+    echo "[info] replacing $current with $1"
+    # Replace the redis URL if the arg $1 url is different than $current
+    sed -ivE "s/server\s(.+?):6379/server $1:6379/" $nginx_config 
     nginx -s reload
     echo "[info] successfully replaced redis url and reloaded nginx config"
 else
-# Don't replace, instead print an error
+    # Don't replace, instead print an error
     echo "[error] couldn't replace $current with $1 since they're the same url"
     exit 1
 fi
