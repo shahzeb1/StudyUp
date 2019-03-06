00885702f7e3f4694e8e85bd5af1a1b3f6f8c79a
Shahzeb K <shahzeb001@gmail.com>
2019-03-05 22:33:03 -0800

    Fixed minor shell script bug

--- docker/nginx-REDIS/hotswap.sh
+++ docker/nginx-REDIS/hotswap.sh
@@ -7,7 +7,7 @@ current=$(sed -En 's/.*server\s(.+?):6379;.*/\1/p' /etc/nginx/nginx.conf)
 echo "[info] replacing $current with $0"
 
 # If statement for extra credit:
-if ["$current" -ne "$0"]
+if ["$current" -ne "$0"]; then
     # Replace the redis URL if the arg $0 url is different than $current
     sed -iE "s/server\s(.+?):6379/server $0:6379/" /etc/nginx/nginx.conf
     nginx -s reload
