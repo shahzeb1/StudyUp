0367e50c727afdce01fc349fc9b1dd8c7047af86
Shahzeb K <shahzeb001@gmail.com>
2019-03-05 23:04:03 -0800

    Debuggig the if statements

--- docker/nginx-REDIS/hotswap.sh
+++ docker/nginx-REDIS/hotswap.sh
@@ -2,24 +2,24 @@
 set -e
 
 # Check to make sure that an argument was passed in
-if [-z "$0"]; then
+if [[ -z "$1" ]]; then
     echo "[error] usage: hotswap.sh <new redis url>"
     exit 1
 fi
 
 # Get the current redis url:
-current=$(sed -En 's/.*server\s(.+?):6379;.*/\1/p' /etc/nginx/nginx.conf) 
+current=$(sed -En 's/.*server\s(.+?):6379;.*/\1/p' /etc/nginx/nginx.conf)
 
-echo "[info] replacing $current with $0"
+echo "[info] replacing $current with $1"
 
 # If statement for extra credit:
-if ["$current" -ne "$0"]; then
-    # Replace the redis URL if the arg $0 url is different than $current
-    sed -iE "s/server\s(.+?):6379/server $0:6379/" /etc/nginx/nginx.conf
+if [[ "$current" -ne "$1" ]]; then
+# Replace the redis URL if the arg $1 url is different than $current
+    sed -ivE "s/server\s(.+?):6379/server $1:6379/" /etc/nginx/nginx.conf
     nginx -s reload
     echo "[info] successfully replaced redis url and reloaded nginx config"
 else
-    # Don't replace, instead print an error
-    echo "[error] couldn't replace $current with $0 since they're the same url"
+# Don't replace, instead print an error
+    echo "[error] couldn't replace $current with $1 since they're the same url"
     exit 1
 fi
